d2e1c135770b216db6e9cc9b440e7dc5c21ddcce
ArtemkaK1 <artemkakirillov04@gmail.com>
2021-02-12 19:53:15 +0300

    .

---
+++ src/TicTacToe.java
@@ -0,0 +1,169 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class TicTacToe {
+    // Человек
+    private static final char DOT_HUMAN = 'X';
+    // Компьютер
+    private static final char DOT_AI = 'O';
+    private static final char DOT_EMPTY = '_';
+    // ход человека
+    private static final Scanner SCANNER = new Scanner(System.in);
+    // ход компьютера
+    private static final Random RANDOM = new Random();
+    // размер поля по X
+    private static int fieldSizeX;
+    // размер поля по Y
+    private static int fieldSizeY;
+    // Поле
+    private static char[][] field;
+    private static int winLength;
+
+    // инциализация поля
+    private static void initField() {
+        fieldSizeX = 3;
+        fieldSizeY = 3;
+        winLength = 3;
+        field = new char[fieldSizeY][fieldSizeX];
+        for (int y = 0; y < fieldSizeY; y++) {
+            for (int x = 0; x < fieldSizeX; x++) {
+                field[y][x] = DOT_EMPTY;
+            }
+        }
+    }
+    // отрисовка поля
+    private static void printField() {
+        System.out.print("+");
+        for (int i = 0; i < fieldSizeX * 2 + 1; i++)
+            System.out.print((i % 2 == 0) ? "-" : i / 2 + 1);
+        System.out.println();
+
+        for (int i = 0; i < fieldSizeY; i++) {
+            System.out.print(i + 1 + "|");
+            for (int j = 0; j < fieldSizeX; j++)
+                System.out.print(field[i][j] + "|");
+            System.out.println();
+        }
+
+        for (int i = 0; i <= fieldSizeX * 2 + 1; i++)
+            System.out.print("-");
+        System.out.println();
+    }
+    // ход человека
+    private static void humanTurn() {
+        int x;
+        int y;
+        do {
+            System.out.print("Введите координаты (вер и гор) от 1 до " + fieldSizeX + " через пробел>> ");
+            x = SCANNER.nextInt() - 1;
+            y = SCANNER.nextInt() - 1;
+        } while (!isCellValid(x, y) || !isCellEmpty(x, y));
+        field[y][x] = DOT_HUMAN;
+    }
+    // ход компьютера
+    private static void aiTurn() {
+        if(turnAIWinCell()) return;
+        if(turnHumanWinCell()) return;
+        int x, y;
+        do {
+            x = RANDOM.nextInt(fieldSizeX);
+            y = RANDOM.nextInt(fieldSizeY);
+        } while (!isCellEmpty(x, y));
+        field[y][x] = DOT_AI;
+    }
+
+    private static boolean turnAIWinCell() {
+        for (int i = 0; i < fieldSizeY; i++) {
+            for (int j = 0; j < fieldSizeX; j++) {
+                if (isCellEmpty(j, i)) {
+                    field[i][j] = DOT_AI;
+                    if (checkWin(DOT_AI)) return true;
+                    field[i][j] = DOT_EMPTY;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static boolean turnHumanWinCell() {
+        for (int i = 0; i < fieldSizeY; i++) {
+            for (int j = 0; j < fieldSizeX; j++) {
+                if (isCellEmpty(j, i)) {
+                    field[i][j] = DOT_HUMAN;
+                    if (checkWin(DOT_HUMAN)) {
+                        field[i][j] = DOT_AI;
+                        return true;
+                    }
+                    field[i][j] = DOT_EMPTY;
+                }
+            }
+        }
+        return false;
+    }
+    // проверка поля на занятость
+    private static boolean isCellEmpty(int x, int y) {
+        return field[y][x] == DOT_EMPTY;
+    }
+    // проверка поля на валидность размер
+    private static boolean isCellValid(int x, int y) {
+        return x >= 0 && y >= 0 && x < fieldSizeX && y < fieldSizeY;
+    }
+    // Проверка победы
+    private static boolean checkWin(int c) {
+        for (int i = 0; i < fieldSizeX; i++) {
+            for (int j = 0; j < fieldSizeY; j++) {
+                if (checkLine(i, j, 1, 0, winLength, c)) return true;
+                if (checkLine(i, j, 1, 1, winLength, c)) return true;
+                if (checkLine(i, j, 0, 1, winLength, c)) return true;
+                if (checkLine(i, j, 1, -1, winLength, c)) return true;
+            }
+        }
+        return false;
+    }
+    private static boolean checkLine(int x, int y, int vx, int vy, int len, int c) {
+        final int far_x = x + (len - 1) * vx;
+        final int far_y = y + (len - 1) * vy;
+        if (!isCellValid(far_x, far_y)) return false;
+        for (int i = 0; i < len; i++) {
+            if (field[y + i * vy][x + i * vx] != c) return false;
+        }
+        return true;
+    }
+    // проверка ничьи
+    private static boolean isDraw() {
+        for (int y = 0; y < fieldSizeY; y++) {
+            for (int x = 0; x < fieldSizeX; x++) {
+                if (field[y][x] == DOT_EMPTY)
+                    return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        initField();
+        printField();
+
+        while (true) {
+            humanTurn();
+            printField();
+            if (checkWin(DOT_HUMAN)) {
+                System.out.println("Победа человека!");
+                break;
+            }
+            if (isDraw()) {
+                System.out.println("Ничья!");
+                break;
+            }
+            aiTurn();
+            printField();
+            if (checkWin(DOT_AI)) {
+                System.out.println("Победа компьютера!");
+                break;
+            }
+            if (isDraw()) {
+                System.out.println("Ничья!");
+                break;
+            }
+        }
+    }
+}
